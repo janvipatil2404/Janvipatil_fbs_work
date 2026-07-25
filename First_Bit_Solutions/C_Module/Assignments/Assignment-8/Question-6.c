86b377ac707d93ca1ded82fee3679335eecde7f6
Janvi patil <janvipatil2404@gmail.com>
2026-07-26 00:53:12 +0530

    Update Question-6.c

--- First_Bit_Solutions/C_Module/Assignments/Assignment-8/Question-6.c
+++ First_Bit_Solutions/C_Module/Assignments/Assignment-8/Question-6.c
@@ -1,4 +1,36 @@
 void main()
 {
-	
+    int arr[10],brr[10],crr[10];
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+
+    if(n<=0 || n>10)
+        printf("Number is greater than array size");
+    else
+    {
+        printf("Enter elements of first array:\n");
+        for(int i=0;i<n;i++)
+            scanf("%d",&arr[i]);
+
+	        printf("Enter elements of second array:\n");
+	        for(int i=0;i<n;i++)
+	            scanf("%d",&brr[i]);
+		
+		        for(int i=0;i<n;i++)
+		            crr[i]=arr[i]+brr[i];
+		       
+			        printf("First array: ");
+			        for(int i=0;i<n;i++)
+			            printf("%d ",arr[i]);
+            
+			        printf("\nSecond array: ");
+			        for(int i=0;i<n;i++)
+			            printf("%d ",brr[i]);
+
+			        printf("\nThird array: ");
+			        for(int i=0;i<n;i++)
+			            printf("%d ",crr[i]);
+
+    }
 }
